03f1fafe090ba190d97b69d7050232109e7a8881
Wessel Frijters <wessel.frijters@gmail.com>
2018-08-11 12:33:51 +0200

    Block Service Provider

--- Assets/Scripts/Blocks/BlockServiceProvider.cs
+++ Assets/Scripts/Blocks/BlockServiceProvider.cs
@@ -6,10 +6,24 @@ public class BlockServiceProvider : MonoBehaviour {
 
     public static BlockServiceProvider instance;
 
+    [SerializeField]
+    private GenericBlockData[] m_Blocks;
 
 
 	// Use this for initialization
 	void Start () {
         instance = this;
 	}
+
+    public GenericBlockData GetBlock(int id)
+    {
+        for(int i = 0; i < m_Blocks.Length; i++)
+        {
+            if(m_Blocks[i].id == id)
+            {
+                return m_Blocks[i];
+            }
+        }
+        return null;
+    }
 }
