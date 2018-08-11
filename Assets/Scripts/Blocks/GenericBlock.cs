e84d70355d863e73df7e31f1be619c9f202d3cdf
Wessel Frijters <wessel.frijters@gmail.com>
2018-08-11 11:40:26 +0200

    Basic Block

---
+++ Assets/Scripts/Blocks/GenericBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Block", menuName = "Block/Create")]
+public class GenericBlock : ScriptableObject {
+
+    [Header("Basics")]
+    [SerializeField]
+    private string m_BlockName;
+    [SerializeField]
+    private Sprite m_Texture;
+
+
+    public Sprite sprite { get { return m_Texture; } }
+    public string blockName { get { return m_BlockName; } }
+}
