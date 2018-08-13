2880bc57a0f753af63aacc8f86356c19fc74f421
Akli <aklitouati@hotmail.com>
2018-08-13 11:59:43 +0200

    Slime swither

--- Assets/Scripts/Player/PlayerContainer.cs
+++ Assets/Scripts/Player/PlayerContainer.cs
@@ -11,27 +11,98 @@ public class PlayerContainer : MonoBehaviour
         large
     }
 
-    Size _Size;
     [SerializeField] private GameObject[] _Players;
+    [SerializeField] private PlayerBehavior[] _PlayersScripts;
     private int _Active;
+
+    private Animator _Animator;
+    private SpriteRenderer _Renderer;
+
+
+    Vector3 _TransformarionPos;
+    bool _FlipX;
 
     private void Start()
     {
-        for (int i = 0; i < _Players.Length; i++)
+        _PlayersScripts = new PlayerBehavior[_Players.Length];
+        for (int i = 0; i < 3; i++)
         {
+            _PlayersScripts[i] = _Players[i].GetComponent<PlayerBehavior>();
             _Players[i].SetActive(false);
         }
-
+        
         _Players[_Active].SetActive(true);
+        _Animator = GetComponent<Animator>();
+        _Renderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         transform.position = _Players[_Active].transform.position;
+        _Renderer.flipX = _PlayersScripts[_Active]._Renderer.flipX;
+        if (Input.GetKeyDown(KeyCode.G) && _PlayersScripts[_Active]._Grounded)
+        {
+            if (_Active < _Players.Length - 1)
+                Grow();
+        }
+        if (Input.GetKeyDown(KeyCode.H) && _PlayersScripts[_Active]._Grounded)
+        {
+            if(_Active > 0)
+            ShrinkTest();
+        }
     }
 
     private void Grow()
     {
+        _TransformarionPos = _Players[_Active].transform.position;
+        _FlipX = _PlayersScripts[_Active]._Renderer.flipX;
+        _Players[_Active].SetActive(false);
+        
 
+        if (_Active == (int)Size.small)
+        {
+            _Animator.Play("1");
+        }
+        if (_Active == (int)Size.medium)
+        {
+            _Animator.Play("2");
+        }
+    }
+
+    public void GrowEnd()
+    {
+        _Active++;
+        _Players[_Active].SetActive(true);
+        _PlayersScripts[_Active].SwitchState(new Idle(_PlayersScripts[_Active]));
+        _PlayersScripts[_Active]._Renderer.flipX = _FlipX;
+        _PlayersScripts[_Active]._AxeRenderer.flipX = _FlipX;
+        _Players[_Active].transform.position = _TransformarionPos;
+    }
+
+
+    private void GrowTest()
+    {
+        _TransformarionPos = _Players[_Active].transform.position;
+        _FlipX = _PlayersScripts[_Active]._Renderer.flipX;
+        _Players[_Active].SetActive(false);
+        _Active++;
+        _Players[_Active].SetActive(true);
+        _PlayersScripts[_Active].SwitchState(new Idle(_PlayersScripts[_Active]));
+        _PlayersScripts[_Active]._Renderer.flipX = _FlipX;
+        _PlayersScripts[_Active]._AxeRenderer.flipX = _FlipX;
+        _Players[_Active].transform.position = _TransformarionPos;
+    }
+
+    private void ShrinkTest()
+    {
+        _TransformarionPos = _Players[_Active].transform.position;
+        _FlipX = _PlayersScripts[_Active]._Renderer.flipX;
+        _Players[_Active].SetActive(false);
+        _Active--;
+        _Players[_Active].SetActive(true);
+        _PlayersScripts[_Active].SwitchState(new Idle(_PlayersScripts[_Active]));
+        _PlayersScripts[_Active]._Renderer.flipX = _FlipX;
+        _PlayersScripts[_Active]._AxeRenderer.flipX = _FlipX;
+        _Players[_Active].transform.position = _TransformarionPos;
     }
 }
